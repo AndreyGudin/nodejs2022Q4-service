5cf397e415b2694a670d16777479915622dfec6e
Andrey Gudin <aeon150791@gmail.com>
2023-02-01 10:03:46 +0400

    feat: create file for album, add message of starting server

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { UserDB } from './db.users';
 
+@Injectable()
 export default class DB {
   users = new UserDB();
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import DB from 'src/db/db';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly service: DB) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.service.users.create(createUserDto);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.service.users.findAll();
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} user`;
+    return this.service.users.findOne(id);
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.service.users.update(id, updateUserDto);
   }
 
   remove(id: string) {
-    return `This action removes a #${id} user`;
+    return this.service.users.delete(id);
   }
 }
