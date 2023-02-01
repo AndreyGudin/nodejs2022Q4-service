d5c00e2ab3c5064c7e94858afd4c03f5a8244475
Andrey Gudin <aeon150791@gmail.com>
2023-02-01 14:21:44 +0400

    feat: add validation for users endpoint

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNotEmpty()
   oldPassword: string;
+
+  @IsNotEmpty()
   newPassword: string;
 }
