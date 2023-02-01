08e1d85f32e9e206377fe04252d32507d4409d39
Andrey Gudin <aeon150791@gmail.com>
2023-02-01 15:54:21 +0400

    feat: add createDTO for track

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,1 +1,13 @@
-export class CreateTrackDto {}
+import { IsInt, IsNotEmpty } from 'class-validator';
+
+export class CreateTrackDto {
+  @IsNotEmpty()
+  name: string;
+  @IsNotEmpty()
+  artistId: string | null;
+  @IsNotEmpty()
+  albumId: string | null;
+  @IsNotEmpty()
+  @IsInt()
+  duration: number;
+}
