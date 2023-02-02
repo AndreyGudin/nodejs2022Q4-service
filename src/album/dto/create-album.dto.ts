af9037721e816fd9f938113feb32a485a3636176
Andrey Gudin <aeon150791@gmail.com>
2023-02-02 11:12:33 +0400

    feat: add dto for album

--- src/album/dto/create-album.dto.ts
+++ src/album/dto/create-album.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAlbumDto {}
+import { IsInt, IsNotEmpty } from 'class-validator';
+
+export class CreateAlbumDto {
+  @IsNotEmpty()
+  name: string;
+  @IsNotEmpty()
+  @IsInt()
+  year: number;
+  @IsNotEmpty()
+  artistId: string | null;
+}
