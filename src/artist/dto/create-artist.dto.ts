12e813362eb0f7b5be450d802783b8221191caab
Andrey Gudin <aeon150791@gmail.com>
2023-02-02 09:57:08 +0400

    feat: add dto for artist

--- src/artist/dto/create-artist.dto.ts
+++ src/artist/dto/create-artist.dto.ts
@@ -1,1 +1,9 @@
-export class CreateArtistDto {}
+import { IsBoolean, IsNotEmpty } from 'class-validator';
+
+export class CreateArtistDto {
+  @IsNotEmpty()
+  name: string;
+  @IsNotEmpty()
+  @IsBoolean()
+  grammy: boolean;
+}
