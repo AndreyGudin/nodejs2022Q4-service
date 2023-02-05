a5116f46e0b9032e4bc1764d5c66c9eb58d7b870
Andrey Gudin <aeon150791@gmail.com>
2023-02-05 10:47:33 +0400

    feat: add env file config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = 4000;
+  const PORT = process.env.PORT || 4001;
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
