be338ad18c94e2c457a84aa62f76f0491a75b841
Andrey Gudin <aeon150791@gmail.com>
2023-02-08 14:45:24 +0400

    feat: add track entity for postgres

--- src/track/entities/track.entity.ts
+++ src/track/entities/track.entity.ts
@@ -1,8 +1,23 @@
+import { Column, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { Album } from 'src/album/entities/album.entity';
+import { Artist } from 'src/artist/entities/artist.entity';
+
 export class Track {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @OneToOne(() => Artist, { onDelete: 'SET NULL' })
+  @JoinColumn()
   artistId: string | null;
+
+  @OneToOne(() => Album, { onDelete: 'SET NULL' })
+  @JoinColumn()
   albumId: string | null;
+  @Column()
   duration: number;
 
   constructor(partial: Partial<Track>) {
