92815b4f0e8edc5cdf1d9304ae2f5f641b2fa306
Andrey Gudin <aeon150791@gmail.com>
2023-02-08 13:14:10 +0400

    feat: add album entity for db

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,12 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Artist } from 'src/artist/entities/artist.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
+@Entity('album')
 export class Album {
+  @PrimaryGeneratedColumn('uuid')
   @ApiProperty()
   id: string;
+
+  @Column()
   @ApiProperty()
   name: string;
+
+  @Column()
   @ApiProperty()
   year: number;
+
+  @OneToOne(() => Artist)
+  @JoinColumn()
   @ApiProperty()
   artistId: string | null;
 
