04a29d384ac368ea2b835bd96a6c0b311d651ec7
Andrey Gudin <aeon150791@gmail.com>
2023-02-12 20:30:33 +0400

    feat: add migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './ormconfig';
 
 @Module({
   imports: [
@@ -17,16 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     AlbumModule,
     FavsModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: +process.env.POSTGRES_PORT,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(ormConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
