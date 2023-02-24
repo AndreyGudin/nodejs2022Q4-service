f79826f0a0efa3faebd165dabec948b0cca403fc
Andrey Gudin <aeon150791@gmail.com>
2023-02-24 19:30:19 +0400

    feat: add auth signup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRE_TIME },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+
+import { User } from 'src/user/entities/user.entity';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userService: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+  async signUp(createUserDto: CreateUserDto) {
+    const createdUser = new User({
+      login: createUserDto.login,
+      password: createUserDto.password,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      version: 1,
+    });
+    const user = await this.userService.findOne({
+      where: { login: createUserDto.login },
+    });
+    if (user) return;
+    const hashPassword = await bcrypt.hash(
+      createUserDto.password,
+      process.env.CRYPT_SALT,
+    );
+    return await this.userService.save({
+      ...createdUser,
+      password: hashPassword,
+    });
+  }
+}
